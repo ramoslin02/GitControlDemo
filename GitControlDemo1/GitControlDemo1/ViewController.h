6cfd46f23c88f0252e147c6d57916f249e21699e
mark.wang <wl1542997454@sina.com>
2015-08-19 15:55:05 +0800

    Initial Commit

---
+++ GitControlDemo1/GitControlDemo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitControlDemo1
+//
+//  Created by ChampOn on 15/8/19.
+//  Copyright (c) 2015年 com.champontech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitControlDemo1/GitControlDemo1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitControlDemo1
+//
+//  Created by ChampOn on 15/8/19.
+//  Copyright (c) 2015年 com.champontech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
